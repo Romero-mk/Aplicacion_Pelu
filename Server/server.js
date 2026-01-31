9e009f5da9365b977e09c134d5a34383002ce6cb
Michael <tu-correo@ejemplo.com>
2026-01-31 17:52:10 -0400

    Limpieza de server.js y cierre de llaves

--- Server/server.js
+++ Server/server.js
@@ -8,32 +8,26 @@ const app = express();
 
 conectarDB();
 
-
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/../public/login.html");
+  res.sendFile(path.join(__dirname, "..", "public", "login.html"));
 });
+
 
 app.use("/api/citas", require("./routes/citas"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/auditoria", require("./routes/auditoria"));
 
 
-
-
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "login.html"));
-  
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
-});
+
 
 
 
